7466a4e83c5d59b2d4a62e4ada1ddc374f5d18d3
notbeer <62919769+notbeer@users.noreply.github.com>
2022-01-16 19:33:15 -0500

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,8 +2,6 @@ const { obfuscateFile, obfuscateFolder, obfuscateDir } = require('../src');
 
 const path = require('path');
 
-(function(){
-    obfuscateFile(path.join(__dirname, 'test-folder', 'obfuscateFile-test.json'));
-    obfuscateFolder(path.join(__dirname, 'test-folder', 'obfuscateFolder-test'));
-    obfuscateDir(path.join(__dirname, 'test-folder', 'obfuscateDir-test'));
-})();
+obfuscateFile(path.join(__dirname, 'test-folder', 'obfuscateFile-test.json'));
+obfuscateFolder(path.join(__dirname, 'test-folder', 'obfuscateFolder-test'));
+obfuscateDir(path.join(__dirname, 'test-folder', 'obfuscateDir-test'));
